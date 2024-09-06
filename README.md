fad868ee6c5e73de27100f81e967e5c444dfaf46
xingpingcn_pw <zzy4on9@outlook.com>
2024-09-06 23:59:41 +0800

    change config

--- README.md
+++ README.md
@@ -120,7 +120,7 @@ netlify中午-官方
 {
     "VERCEL":"76.76.21.21",
     "NETLIFY":"75.2.60.5",
-    "CF":"visa.cn."
+    "CF":"japan.com."
 }
 ```
 
--- config.py
+++ config.py
@@ -10,7 +10,7 @@ RES_BACKUP_LENGTH = 6
 CNAME_DEFAULT_RECORD = {
     'VERCEL':'76.76.21.21',
     'NETLIFY':'75.2.60.5',
-    'CF':'visa.cn.'
+    'CF':'japan.com.'
 }
 CONCURRENCY = 7 # for aiohttp.TCPConnector(limit=CONCURRENCY)
 NETLIFY_URL_TO_TEST = ['https://domain.com/test.html'] # for get netlify Anycast IPs
